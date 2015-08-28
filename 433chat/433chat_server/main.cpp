349824db0d6fc21bf8184e448ad5f1268cdb7378
ljhoo7 <ljhoo8@naver.com>
2015-08-28 18:29:01 +0900

    퇴근 직전
    ㅇㅇ

--- 433chat/433chat_server/main.cpp
+++ 433chat/433chat_server/main.cpp
@@ -1,82 +1,115 @@
-/////////////////////////////////////////
-// Server.cpp
-// 다음은 서버 프로그램입니다.
-// 물론 Win32 프로그램으로 작성할 땐 Thread를 이용해 빠져야 할 부분입니다.
-#define WIN32_LEAN_AND_MEAN
-// windows.h 파일에서 자주 사용하지 않은 요소를 컴파일에서 제외시킨다.
-// 컴파일 속도가 빨라지고 winsock2.h와의 충돌이 일어나지 않는다.
-#include <iostream>
-#include <windows.h>
-#include <winsock2.h>
-#include "..\skel.h"
-using namespace std;
-#pragma comment ( lib, "ws2_32.lib" )
-#pragma pack( 1 ) // 구조체에서 1byte 단위로 메모리를 Align하라는 지시어
-void Reverse(REV_STRUCT *st) {
-	int size = ntohl(st->size) - 4;
-	for (int i = 0; i < size / 2; ++i) {
-		char temp = st->p[i];
-		st->p[i] = st->p[size - 2 - i];
-		st->p[size - 2 - i] = temp;
+// WindowsSelectServer.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "../skel.h"
+
+void error_handling(const char *buf);
+void paket_handling(t_packet pkt, int i, fd_set reads);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET serv_sock, clnt_sock;
+	SOCKADDR_IN serv_adr, clnt_adr;
+	TIMEVAL timeout;
+	fd_set reads, cpy_reads;
+	t_packet buf;
+
+	int adr_sz;
+	int str_len, fd_num, i, m;
+
+	if(argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
 	}
-}
-DWORD WINAPI ReverseString(void* p) {
-	SOCKET s = (SOCKET)p;
-	REV_STRUCT pkt = { 0 };
 
-	int current = 0;
-	int n = recv(s, (char*)&pkt, 4, 0);
-	int total = ntohl(pkt.size);
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error_handling("WSAStartup() error!");
 
-	current += n;
-	while (total > current) {
-		n = recv(s, (char*)&pkt + current, total - current, 0);
-		if (n > 0) {
-			current += n;
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == SOCKET_ERROR)
+		error_handling("bind() error");
+	if (listen(serv_sock, 5) == SOCKET_ERROR)
+		error_handling("listen() error");
+
+	FD_ZERO(&reads);
+	FD_SET(serv_sock, &reads);
+
+	while (1)
+	{
+		cpy_reads = reads;
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 5000;
+
+		if ((fd_num = select(0, &cpy_reads, 0, 0, &timeout)) == SOCKET_ERROR)
+			break;
+		if (fd_num == 0)
 			continue;
+		for (i = 0; i < reads.fd_count; i++)
+		{
+			int sockNum = reads.fd_array[i];
+			if (FD_ISSET(sockNum, &cpy_reads))
+			{
+				if (sockNum == serv_sock)
+				{
+					adr_sz = sizeof(clnt_adr);
+					clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+					FD_SET(clnt_sock, &reads);
+					printf("connected client: %d\n", i);
+				}
+				else
+				{
+					str_len = recv(sockNum, (char*)&buf, sizeof(t_packet), 0);
+					if (str_len == 0)
+					{
+						FD_CLR(sockNum, &reads);
+						closesocket(sockNum);
+						printf("closed client:%d\n", i);
+					}
+					else
+					{
+						paket_handling(buf, i, reads);
+					}
+				}
+			}
 		}
-		else if (n <= 0) {
-			cout << "Error 발생 : " << WSAGetLastError() << endl;
-			return -1;
-		}
-	} // while
-	Reverse(&pkt);
-	send(s, (char*)&pkt, total, 0);
+	}
+
 	return 0;
 }
-void main() {
-	WSADATA wsadata;
-	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
-		cout << "winsock 초기화 실패 : " << WSAGetLastError() << endl;
-		return;
-	} // if
-	//----------------------------------------------------------
-	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (s == SOCKET_ERROR) {
-		cout << "소켓이 생성되지 않았습니다." << endl;
-		return;
+
+void paket_handling(t_packet pkt, int i, fd_set reads)
+{
+	int m;
+	t_packet result_pkt;
+	switch (pkt.m_any.type)
+	{
+	case pt_move:
+		printf("move : (%f, %f, %f)=>(%f, %f, %f)\n", pkt.m_move.start_coord.x, pkt.m_move.start_coord.y, pkt.m_move.start_coord.z, pkt.m_move.dest_coord.x, pkt.m_move.dest_coord.y, pkt.m_move.dest_coord.z);
+
+		pkt.m_move.time = 0;
+
+		for (m = 0; m < reads.fd_count; ++m)
+		{
+			if (m != i)
+			{
+				printf("%d's moving message was sent to %d\n", i, m);
+				send(reads.fd_array[m], (char*)&pkt, sizeof(t_packet), 0);
+			}
+		}
+		break;
 	}
-	SOCKADDR_IN addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(4500);
-	addr.sin_addr.s_addr = INADDR_ANY;
-	if (bind(s, (SOCKADDR*)&addr, sizeof(addr)) == -1) {
-		cout << "Failed binding" << endl;
-		return;
-	}
-	if (listen(s, 5) == -1) {
-		cout << "Failed listening" << endl;
-		return;
-	}
-	cout << "Waiting for connencting from Client..." << endl;
-	while (1) {
-		int size = sizeof(addr);
-		SOCKET c_s = accept(s, (SOCKADDR*)&addr, &size);
-		cout << "접속된 Client : " << inet_ntoa(addr.sin_addr) << endl;
-		CloseHandle(CreateThread(0, 0, ReverseString, (void*)c_s, 0, 0));
-	} // while( 1 )
-	closesocket(s);
-	//----------------------------------------------------------
-	WSACleanup();
 }
-//////////////////////////////////////////////////////////////////
+
+void error_handling(const char *buf)
+{
+	fputs(buf, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
--- 433chat/skel.h
+++ 433chat/skel.h
@@ -1,11 +1,66 @@
 ////////////////////////////////////////
 // 다음은 서버와 클라이언트 간 데이터를 주고 받을 때 사용하는 패킷을 정의한 헤더입니다.
-#ifndef _REVSTRUCT_H
-#define _REVSTRUCT_H
-#pragma pack( 1 )
-typedef struct _REV_STRUCT{
- int size;
- char p[256];
-} REV_STRUCT;
-#endif
-///////////////////////////////////////
+#pragma once
+
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#define PKTLEN 512
+
+enum pkt_type { pt_any, pt_create, pt_destroy, pt_join, pt_leave, pt_chat };
+
+typedef struct{
+	float x, y, z;
+}USER_COORD;
+
+typedef struct{
+	short length;
+	short type;
+	char payload[PKTLEN - 4];
+}t_any;
+
+typedef struct{
+	short length;
+	short type;
+
+}t_create;
+
+typedef struct{
+	short length;
+	short type;
+
+}t_destroy;
+
+typedef struct{
+	short length;
+	short type;
+
+}t_join;
+
+typedef struct{
+	short length;
+	short type;
+
+}t_leave;
+
+typedef struct{
+	short length;
+	short type;
+
+
+
+}t_chat;
+
+typedef union{
+	t_any		m_any;
+	t_create	m_create;
+	t_destroy	m_destroy;
+	t_join		m_join;
+	t_leave		m_leave;
+	t_chat		m_chat;
+}t_packet;
