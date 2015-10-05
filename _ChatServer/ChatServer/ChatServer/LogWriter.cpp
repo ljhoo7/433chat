13a0d8f66db396f3642f4fa301e073b0c9a18fa2
yumas <ot8844@gmail.com>
2015-10-05 15:53:16 +0900

    로그 writer 구현

---
+++ _ChatServer/ChatServer/ChatServer/LogWriter.cpp
@@ -0,0 +1,72 @@
+#include "stdafx.h"
+
+LogWriter::LogWriter(PTCH p_szStr, int p_nNumOfThreads)
+:m_nNumOfThreads(p_nNumOfThreads)
+{
+	m_hLogIOCP = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, m_nNumOfThreads);
+	
+	if (NULL == m_hLogIOCP)
+	{
+		BeforeMakingIOCPMessage(L"The OS has failed to make a IOCP kernel object for the log file!\n");
+	}
+
+	for( int i = 0; i < m_nNumOfThreads; ++i)
+	{
+		unsigned int t_unThreadId;
+		if (-1 == _beginthreadex(NULL, 0, LogWriter::ThreadProc, (void *)this, 0, &t_unThreadId))
+		{
+			BeforeMakingIOCPMessage(L"The OS has failed to make a worker thread for log file!\n");
+		}
+	}
+
+	m_pLogAct = new CLogAct(p_szStr);
+
+	if (NULL == ::CreateIoCompletionPort(m_pLogAct->m_cFile.m_hFile, m_hLogIOCP, 0, 0))
+	{
+		BeforeMakingIOCPMessage(L"The OS has failed to assign the log file to the IOCP !\n");
+	}
+
+	unsigned short	t_usMark = 0xFEFF;
+	DWORD			t_dwNumberOfBytesWritten;
+	int t_nResult;
+
+	// For Writing the Unicode Mark
+	t_nResult = ::WriteFile(m_pLogAct->m_cFile.m_hFile, &t_usMark, sizeof(t_usMark), &t_dwNumberOfBytesWritten, m_pLogAct);
+
+	if (!t_nResult)
+	{
+		BeforeMakingIOCPMessage(L"The OS has failed to write the unicode mark on the log file !\n");
+	}
+}
+
+LogWriter::~LogWriter()
+{
+	CloseHandle(m_hLogIOCP);
+	delete m_pLogAct;
+}
+
+UINT WINAPI LogWriter::ThreadProc(PVOID p_pRaram)
+{
+	// Check The Finish of an Writing
+	return 0;
+}
+
+int LogWriter::wprintf_s(PTCH p_szStr)
+{
+#ifdef _DEBUG
+	if(0 > wprintf_s(p_szStr))
+		BeforeMakingIOCPMessage(L"The LogWriter has failed to write a message in debug mode.\n");
+	exit(1);
+#else
+	DWORD			t_dwNumberOfBytesWritten;
+	int t_nResult;
+
+	// For Writing the Unicode Mark
+	t_nResult = ::WriteFile(m_pLogAct->m_cFile.m_hFile, p_szStr, wcslen(p_szStr) * sizeof(TCHAR), &t_dwNumberOfBytesWritten, m_pLogAct);
+
+	if (!t_nResult)
+	{
+		BeforeMakingIOCPMessage(L"The LogWriter has failed to write a message in release mode.\n");
+	}
+#endif
+}
---
+++ _ChatServer/ChatServer/ChatServer/LogWriter.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "LogAct.h"
+
+class LogWriter
+{
+	HANDLE m_hLogIOCP;
+
+	int m_nNumOfThreads;
+
+	CLogAct *m_pLogAct;
+public:
+	static UINT WINAPI ThreadProc(PVOID p_pRaram);
+
+	LogWriter(PTCH p_szStr, int p_nNumOfThreads);
+	~LogWriter();
+
+	int wprintf_s(PTCH p_szStr);
+};
